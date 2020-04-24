5538655f572bd7b9b3872ed9f9d0aba86870ac63
miroslav-sc <miroslavs@safecharge.com>
2020-04-24 11:57:12 +0300

    added more validations for the request parameters

--- safecharge/SC_HELPER.php
+++ safecharge/SC_HELPER.php
@@ -3,13 +3,121 @@
 /**
  * SC_HELPER Class
  * 
- * @year 2019
+ * @year 2020
  * @author SafeCharge
  */
 class SC_HELPER
 {
+	// array details to validate request parameters
+    private $params_validation = array(
+        // deviceDetails
+        'deviceType' => array(
+            'length' => 10,
+            'flag'    => FILTER_SANITIZE_STRING
+        ),
+        'deviceName' => array(
+            'length' => 255,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'deviceOS' => array(
+            'length' => 255,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'browser' => array(
+            'length' => 255,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'ipAddress' => array(
+            'length' => 15,
+            'flag'    => FILTER_VALIDATE_IP
+        ),
+        // deviceDetails END
+        
+        // userDetails, shippingAddress, billingAddress
+        'firstName' => array(
+            'length' => 30,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'lastName' => array(
+            'length' => 40,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'address' => array(
+            'length' => 60,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'cell' => array(
+            'length' => 18,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'phone' => array(
+            'length' => 18,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'zip' => array(
+            'length' => 10,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'city' => array(
+            'length' => 30,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'country' => array(
+            'length' => 20,
+            'flag'    => FILTER_SANITIZE_STRING
+        ),
+        'state' => array(
+            'length' => 2,
+            'flag'    => FILTER_SANITIZE_STRING
+        ),
+        'email' => array(
+            'length' => 100,
+            'flag'    => FILTER_VALIDATE_EMAIL
+        ),
+        'county' => array(
+            'length' => 255,
+            'flag'    => FILTER_DEFAULT
+        ),
+        // userDetails, shippingAddress, billingAddress END
+        
+        // specific for shippingAddress
+        'shippingCounty' => array(
+            'length' => 255,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'addressLine2' => array(
+            'length' => 50,
+            'flag'    => FILTER_DEFAULT
+        ),
+        'addressLine3' => array(
+            'length' => 50,
+            'flag'    => FILTER_DEFAULT
+        ),
+        // specific for shippingAddress END
+        
+        // urlDetails
+        'successUrl' => array(
+            'length' => 1000,
+            'flag'    => FILTER_VALIDATE_URL
+        ),
+        'failureUrl' => array(
+            'length' => 1000,
+            'flag'    => FILTER_VALIDATE_URL
+        ),
+        'pendingUrl' => array(
+            'length' => 1000,
+            'flag'    => FILTER_VALIDATE_URL
+        ),
+        'notificationUrl' => array(
+            'length' => 1000,
+            'flag'    => FILTER_VALIDATE_URL
+        ),
+        // urlDetails END
+    );
+	
     /**
      * Function call_rest_api
+	 * 
      * Call REST API with cURL post and get response.
      * The URL depends from the case.
      * 
@@ -20,15 +128,51 @@ class SC_HELPER
      */
     public static function call_rest_api($url, $params)
     {
-        $resp = false;
-        
-        // get them only if we pass them empty
-        if(isset($params['deviceDetails']) && empty($params['deviceDetails'])) {
-            $params['deviceDetails'] = self::get_device_details();
-        }
-        
 		self::create_log($url, 'REST API URL:');
-        self::create_log($params, 'SC_REST_API, parameters for the REST API call:');
+		self::create_log($params, 'SC_REST_API, parameters for the REST API call:');
+		
+		if (empty($url)) {
+			self::create_log('SC_REST_API, the URL is empty!');
+			return false;
+		}
+		
+        $resp = false;
+		
+		// get them only if we pass them empty
+		if (empty($params['deviceDetails'])) {
+			$params['deviceDetails'] = self::get_device_details();
+		}
+		
+		// validate parameters
+		foreach ($params as $key1 => $val1) {
+            if (!is_array($val1) && !empty($val1) && array_key_exists($key1, $this->params_validation)) {
+                $new_val = $val1;
+                
+                if (mb_strlen($val1) > $this->params_validation[$key1]['length']) {
+                    $new_val = mb_substr($val1, 0, $this->params_validation[$key1]['length']);
+                    
+                    self::create_log($key1, 'Limit');
+                }
+                
+                $params[$key1] = filter_var($new_val, $this->params_validation[$key1]['flag']);
+            }
+			elseif (is_array($val1) && !empty($val1)) {
+                foreach ($val1 as $key2 => $val2) {
+                    if (!is_array($val2) && !empty($val2) && array_key_exists($key2, $this->params_validation)) {
+                        $new_val = $val2;
+
+                        if (mb_strlen($val2) > $this->params_validation[$key2]['length']) {
+                            $new_val = mb_substr($val2, 0, $this->params_validation[$key2]['length']);
+                            
+                            self::create_log($key2, 'Limit');
+                        }
+
+                        $params[$key1][$key2] = filter_var($new_val, $this->params_validation[$key2]['flag']);
+                    }
+                }
+            }
+        }
+		// validate parameters END
         
         $json_post = json_encode($params);
         
@@ -50,25 +194,26 @@ class SC_HELPER
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
             $resp = curl_exec($ch);
-            curl_close ($ch);
+			curl_close($ch);
+			
+			if (false === $resp) {
+				return false;
+			}
+
+			$resp_arr = json_decode($resp, true);
+			self::create_log($resp_arr, 'REST API response: ');
+
+			return $resp_arr;
         }
         catch(Exception $e) {
             self::create_log($e->getMessage(), 'Exception ERROR when call REST API: ');
             return false;
         }
-        
-        if($resp === false) {
-            return false;
-        }
-        
-        $resp_arr = json_decode($resp, true);
-        self::create_log($resp_arr, 'REST API response: ');
-
-        return $resp_arr;
     }
     
     /**
      * Function get_device_details
+	 * 
      * Get browser and device based on HTTP_USER_AGENT.
      * The method is based on D3D payment needs.
      * 
@@ -77,86 +222,92 @@ class SC_HELPER
     public static function get_device_details()
     {
         $device_details = array(
-            'deviceType'    => 'UNKNOWN', // DESKTOP, SMARTPHONE, TABLET, TV, and UNKNOWN
-            'deviceName'    => '',
-            'deviceOS'      => '',
-            'browser'       => '',
-            'ipAddress'     => '',
-        );
-        
-        if(!isset($_SERVER['HTTP_USER_AGENT']) || empty(isset($_SERVER['HTTP_USER_AGENT']))) {
-            return $device_details;
-        }
-        
-        $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
-        
-        $device_details['deviceName'] = substr_compare($_SERVER['HTTP_USER_AGENT'], 0, 250);
+			'deviceType'    => 'UNKNOWN', // DESKTOP, SMARTPHONE, TABLET, TV, and UNKNOWN
+			'deviceName'    => 'UNKNOWN',
+			'deviceOS'      => 'UNKNOWN',
+			'browser'       => 'UNKNOWN',
+			'ipAddress'     => '0.0.0.0',
+		);
+		
+		if(empty($_SERVER['HTTP_USER_AGENT'])) {
+			$device_details['Warning'] = 'User Agent is empty.';
+			
+			self::create_log($device_details['Warning'], 'Error');
+			return $device_details;
+		}
+		
+		$user_agent = strtolower(filter_var($_SERVER['HTTP_USER_AGENT'], FILTER_SANITIZE_STRING));
+		
+		if (empty($user_agent)) {
+			$device_details['Warning'] = 'Probably the merchant Server has problems with PHP filter_var function!';
+			
+			self::create_log($device_details['Warning'], 'Error');
+			return $device_details;
+		}
+		
+		$device_details['deviceName'] = $user_agent;
+		
+        if (defined('SC_DEVICES_TYPES')) {
+			$devs_tps = json_decode(SC_DEVICES_TYPES, true);
 
-        if(defined('SC_DEVICES_TYPES')) {
-            $devs_tps = json_decode(SC_DEVICES_TYPES, true);
-
-            if(is_array($devs_tps) && !empty($devs_tps)) {
-                foreach ($devs_tps as $d) {
-                    if (strstr($user_agent, $d) !== false) {
-                        if(in_array($d, array('linux', 'windows', 'macintosh'), true)) {
-                            $device_details['deviceType'] = 'DESKTOP';
-                        }
-						else if('mobile' === $d) {
+			if (is_array($devs_tps) && !empty($devs_tps)) {
+				foreach ($devs_tps as $d) {
+					if (strstr($user_agent, $d) !== false) {
+						if(in_array($d, array('linux', 'windows', 'macintosh'), true)) {
+							$device_details['deviceType'] = 'DESKTOP';
+						} else if('mobile' === $d) {
 							$device_details['deviceType'] = 'SMARTPHONE';
-						}
-						else if('tablet' === $d) {
+						} else if('tablet' === $d) {
 							$device_details['deviceType'] = 'TABLET';
-						}
-						else {
+						} else {
 							$device_details['deviceType'] = 'TV';
 						}
 
-                        break;
-                    }
-                }
-            }
-        }
+						break;
+					}
+				}
+			}
+		}
 
-        if(defined('SC_DEVICES')) {
-            $devs = json_decode(SC_DEVICES, true);
+        if (defined('SC_DEVICES')) {
+			$devs = json_decode(SC_DEVICES, true);
 
-            if(is_array($devs) && !empty($devs)) {
-                foreach ($devs as $d) {
-                    if (strstr($user_agent, $d) !== false) {
-                        $device_details['deviceOS'] = $d;
-                        break;
-                    }
-                }
-            }
-        }
+			if (is_array($devs) && !empty($devs)) {
+				foreach ($devs as $d) {
+					if (strstr($user_agent, $d) !== false) {
+						$device_details['deviceOS'] = $d;
+						break;
+					}
+				}
+			}
+		}
 
-        if(defined('SC_BROWSERS')) {
-            $brs = json_decode(SC_BROWSERS, true);
+		if (defined('SC_BROWSERS')) {
+			$brs = json_decode(SC_BROWSERS, true);
 
-            if(is_array($brs) && !empty($brs)) {
-                foreach ($brs as $b) {
-                    if (strstr($user_agent, $b) !== false) {
-                        $device_details['browser'] = $b;
-                        break;
-                    }
-                }
-            }
-        }
+			if (is_array($brs) && !empty($brs)) {
+				foreach ($brs as $b) {
+					if (strstr($user_agent, $b) !== false) {
+						$device_details['browser'] = $b;
+						break;
+					}
+				}
+			}
+		}
 
         // get ip
-        $ip_address = '';
-
-        if (isset($_SERVER["REMOTE_ADDR"])) {
-            $ip_address = $_SERVER["REMOTE_ADDR"];
-        }
-        elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-            $ip_address = $_SERVER["HTTP_X_FORWARDED_FOR"];
-        }
-        elseif (isset($_SERVER["HTTP_CLIENT_IP"])) {
-            $ip_address = $_SERVER["HTTP_CLIENT_IP"];
-        }
-
-        $device_details['ipAddress'] = (string) $ip_address;
+		if (!empty($_SERVER['REMOTE_ADDR'])) {
+			$ip_address = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP);
+		}
+		if (empty($ip_address) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$ip_address = filter_var($_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP);
+		}
+		if (empty($ip_address && !empty($_SERVER['HTTP_CLIENT_IP']))) {
+			$ip_address = filter_var($_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP);
+		}
+		if (!empty($ip_address)) {
+			$device_details['ipAddress'] = (string) $ip_address;
+		}
             
         return $device_details;
     }
